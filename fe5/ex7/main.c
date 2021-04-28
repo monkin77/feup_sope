cde62429dbf859f9a801148a67b7f85da957fe1b
monkin77 <monkinsmurf@gmail.com>
2021-04-28 17:07:24 +0100

    update

--- fe5/ex7/main.c
+++ fe5/ex7/main.c
@@ -11,8 +11,8 @@ int main() {
     
     printf("\nInsert your username\n");
 
-	char username[100];
-	char password[100];
+	char username[10000];
+	char password[10000];
 	int userIdx = 0;
 	int passIdx = 0;
 
@@ -22,20 +22,41 @@ int main() {
 		else{
 			username[userIdx] = c;
 			userIdx++;
-			printf("***");
+		}
+		} while (c != '\n');	// wait for [ENTER]
+
+	tms.c_lflag &= ~ICANON; // Disable Canonical mode (Start reading 1 char at a time)
+	tms.c_lflag &= ~ECHO; // inhibit char echoing (Since we want to show '*' instead)
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &tms); // set new configuration
+
+	printf("Insert your password\n");
+
+	do {
+		if (read(STDIN_FILENO, &c, 1) != 1)
+			perror("read failed");
+		else{
+			if(c == 0x08){
+				printf("backspace\n");
+				passIdx--;
+				password[passIdx] = ' ';
+			}
+			else{
+				password[passIdx] = c;
+				passIdx++;
+			}
+			write(STDIN_FILENO, &c, 1);
+			write(STDIN_FILENO, "***", 3);
 		}
 		} while (c != '\n');	// wait for [ENTER]
 
 	printf("\nUsername: %s", username);
+	printf("\nPassword: %s", password);
 
-    /* 
-
-	tms.c_lflag &= ~ECHO; // inhibit char echoing
-	tcsetattr(STDIN_FILENO, TCSANOW, &tms); // set new configuration
-	*/
 
 	tcsetattr(STDIN_FILENO, TCSANOW, &tms_ini);	// reset configuration
 	// experiment this program with the above line commented! ;-)
+
 	printf("\nInitial console configuration has been reset!\n");
 
 	
